82f543817dda659cbad79f9d44ba50da416af94c
CookieSheet <76230707+CookieSheet@users.noreply.github.com>
2025-05-15 11:11:17 -0700

    fixed dockerfile endpoint call

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 5555
 
 # Command to run your application
-CMD ["python", "your_microservice.py"]
+CMD ["python", "microserviceA.py"]
--- client.py
+++ client.py
@@ -2,11 +2,11 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+socket.connect("tcp://microservicea.fly.dev:5555")
 
 print("Sending first location request...")
 socket.send_string("San Francisco, CA")
-message = socket.recv()
+message = socket.recv_string()
 print(f"Server replied: {message.decode()}")
 
 print("Sending shutdown signal...")
